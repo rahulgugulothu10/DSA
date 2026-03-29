aa3818cc77c006f513e1143e4b700bbc3b3ca6dc
Gugulothu-Rahul <rahulgugulothu744@gmail.com>
2026-03-29 22:08:29 +0530

    Time: 2 ms (17.29%) | Memory: 42.6 MB (44.26%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set=new HashSet<>();
+        while(n!=1){
+            if(set.contains(n)){
+                return false;
+            }
+            set.add(n);
+        
+        int sum=0;
+        while(n>0){
+            int dig=n%10;
+            sum+=(dig*dig);
+            n=n/10;
+            
+            }
+            n=sum;
+        }
+        
+       return true;
+        
+    }
+}
