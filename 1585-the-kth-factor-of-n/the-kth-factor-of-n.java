42f131a4bfd7bac736c53600a003392ec38b87ed
Gugulothu-Rahul <rahulgugulothu744@gmail.com>
2026-06-30 21:09:27 +0530

    Time: 1 ms (42.02%) | Memory: 42.1 MB (70.04%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        List<Integer> list=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+           if(n%i==0){
+            list.add(i);
+           }
+        }
+        for(int i=0;i<list.size();i++){
+               if(k==i+1){
+                return list.get(i);
+               }
+        }
+        return -1;
+    }
+}
